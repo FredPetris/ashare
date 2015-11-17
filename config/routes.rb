e0b6bcfcc9eea6785409b83e2ad03acb7afc0552
Frederique Petris <frederique.petris@gmail.com>
2015-11-17 18:02:47 +0100

    add nested ressources to event

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-
-
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
-  resources :events
+  resources :events do
+    resources :pictures, only: [:new, :create, :destroy]
+    resources :occurrences, only: [:new, :create, :destroy]
+  end
+
 end
