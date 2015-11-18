c41db1839166b816285994a7671c0eacedf5d9e0
arnaud huguenin <hhuguenin.arnaud@gmail.com>
2015-11-18 15:31:55 +0100

    edit profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
 
   devise :omniauthable, omniauth_providers: [:facebook]
 
+
   def self.find_for_facebook_oauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
