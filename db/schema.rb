56c8a94b893ee70ef04ce2ae68631593178c67fa
arnaud huguenin <hhuguenin.arnaud@gmail.com>
2015-11-19 17:10:34 +0100

    add lat/long to homes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119160610) do
+ActiveRecord::Schema.define(version: 20151119160928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 20151119160610) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "occurrences", force: :cascade do |t|
